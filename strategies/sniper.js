6f41d2754e068bb25c486b51321349f5bf16ac46
Jaro <j.zink@outlook.com>
2018-04-13 11:48:20 +0200

    qip

--- strategies/sniper.js
+++ strategies/sniper.js
@@ -6,37 +6,53 @@ function addPercent(value, percent) {
     return value*((100+percent)/100)
 }
 
+function diffPercent(value1, value2) {
+  return 100-((value2/value1)*100)
+}
+
 // Let's create our own strategy
 var strat = {};
 
 // Prepare everything our strat needs
 strat.init = function() {
-  this.requiredHistory = 1;
-  this.lastPrice = 0;
-  this.sma = new SMA(30);
-  this.sma5000 = new SMA(5000);
-  this.bought = false;
+  this.requiredHistory = 1
+  this.lastPrice = 0
+  this.sma = new SMA(30)
+  this.sma5000 = new SMA(5000)
+  this.bought = false
+  this.down = 0
 }
 
 // What happens on every new candle?
 strat.update = function(candle) {
-  this.sma.update(candle.close);
-  this.sma5000.update(candle.close);
+  this.sma.update(candle.close)
+  this.sma5000.update(candle.close)
   if (!this.bought) {
-    if (candle.low < addPercent(candle.open, this.settings.spike)) {
-      if (this.sma.result > candle.close) {
-      if (this.sma5000.result > candle.close) {
-          this.advice('long')
-          this.lastPrice = candle.close
-          this.bought = true
-      }
-      }
+    if (candle.low < addPercent(candle.open, -2)) {
+      this.down -= diffPercent(candle.open, candle.low)
     }
+    else if (candle.open > candle.close) {
+      if (this.down < this.settings.spike) {
+        if (this.sma.result > candle.close) {
+          if (this.sma5000.result > candle.close) {
+            this.advice('long')
+            this.lastPrice = candle.close
+            this.bought = true
+          }
+        }
+      }
+      this.down = 0
+    }
+    /*else {
+      this.down = 0
+    }*/
   }
   else if (this.bought) {
-    if (candle.close < addPercent(this.lastPrice, this.settings.limit)) {
+    if (candle.close < this.lastPrice) {
+    //if (candle.close < addPercent(this.lastPrice, this.settings.limit)) {
       this.advice('short')
       this.bought = false
+      this.lastPrice = 0
     }
     else if (candle.close > this.lastPrice) {
       this.lastPrice = candle.close

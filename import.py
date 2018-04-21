7ae3185d7fa05b2962058776759d8daa625c2b16
Jaro <j.zink@outlook.com>
2018-04-21 14:11:39 +0200

    import all data, in batches

--- import.py
+++ import.py
@@ -1,17 +1,47 @@
 import json
 import requests
+import datetime
+
+from time import sleep
+
+import ccxt
 
 MARKETS_FILE = 'exchanges/binance-markets.json'
-ENDPOINT = 'http://localhost:3000/api/import'
+HOST = 'http://localhost:3000/api'
+BATCH_SIZE = 5
+
+binance = ccxt.binance()
 
 markets = json.load(open(MARKETS_FILE))['markets']
+real_markets = binance.load_markets()
 
+def get_first_candle_date(pair: str):
+    timestamp = binance.fetch_ohlcv(pair, since=978307200, limit=1)[0][0] / 1000
+    date_obj =  datetime.datetime.fromtimestamp(timestamp)
+    return date_obj.strftime('%Y-%m-%d %H:%M')
+
+def get_now_str():
+    return datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
+
+def wait_for_imports(imports: list):
+    running = requests.get(HOST + '/imports').json()
+    for item in running:
+        if item['id'] in imports and not item.get('done', False):
+            sleep(1)
+            wait_for_imports(imports)
+            return
+
+imports = []
+count = 0
 for market in markets:
     currency = market['pair'][0]
     coin = market['pair'][1]
+    pair = coin + '/' + currency
+    if not pair in real_markets:
+        continue
     if currency == 'USDT' or currency == 'BTC': # discard ETH and BNB markets
         r = requests.post(
-            ENDPOINT,
+            HOST + '/import',
             headers = {
                 'Content-Type': 'application/json'
             },
@@ -23,8 +53,8 @@ for market in markets:
                 },
                 'importer': {
                     'daterange': {
-                        'from': '2017-08-07 12:00',
-                        'to': '2018-04-13 12:00'
+                        'from': get_first_candle_date(pair),
+                        'to': get_now_str()
                     }
                 },
                 'candleWriter': {
@@ -34,3 +64,10 @@ for market in markets:
         )
         if r.status_code != 200:
             print(coin + '/' + currency + ' could not be imported: ' + r.text)
+            continue
+        imports.append(r.json()['id'])
+        count += 1
+        if count >= BATCH_SIZE:
+            wait_for_imports(imports)
+            imports = []
+            count = 0
